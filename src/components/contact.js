b65bdc81730d3c67a87c748b5af9b63d84027a7e
abhisekmaiti19 <63227192+abhisekmaiti19@users.noreply.github.com>
2020-11-08 17:59:25 +0530

    duplicate contact

---
+++ src/components/contact.js
@@ -0,0 +1,44 @@
+import React from "react"
+import {  graphql } from 'gatsby'
+
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from '../components/Reusable/HeroSection'
+import Infoblock from '../components/Reusable/Infoblock'
+import DualInfoblock from '../components/Reusable/DualInfoblock'
+import Teamphotosection from '../components/About/Teamphotosection'
+
+
+
+const AboutPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+   <HeroSection 
+   img={data.img.childImageSharp.fluid}
+   
+   subtitle=""
+   heroclass="about-background"
+   />
+   <DualInfoblock heading="Massage from Developer"/>
+   <Infoblock heading="About our vision"/> 
+   <Teamphotosection/>
+  
+
+  </Layout>
+)
+
+export const query = graphql`
+{
+  img: file(relativePath: {eq:"5.png"})
+  {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
+
+export default AboutPage

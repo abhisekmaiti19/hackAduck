72ab045d398be9c19771976ffcbf001e5540b673
abhisekmaiti19 <63227192+abhisekmaiti19@users.noreply.github.com>
2021-04-28 23:45:43 +0530

    fixedbug

--- src/components/Reusable/Footer.js
+++ src/components/Reusable/Footer.js
@@ -2,20 +2,25 @@
 //rcc => react class component
 
 import React from "react"
-import { FaFacebook } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaGlobe } from "react-icons/fa";
-import { FaDiscord } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+import { FaGlobe } from "react-icons/fa"
+import { FaDiscord } from "react-icons/fa"
 
-export default function Footer( ) {
+export default function Footer() {
   return (
-    
     <footer className="site-footer">
       <div className="container">
         <div className="row">
           <div className="col-sm-12 col-md-6">
             <h6>About</h6>
-            <p className="text-justify">Launched in Kolkata a month ago, Quick Store has grown from a home project to one of the Hackathon. We are presently develope on more features. We not only connect people to food and their essentials in every context but work closely with Groccerry shops to enable a sustainable ecosystem.</p>
+            <p className="text-justify">
+              Launched in Kolkata a month ago, Quick Store has grown from a home
+              project to one of the Hackathon. We are presently develope on more
+              features. We not only connect people to food and their essentials
+              in every context but work closely with Groccerry shops to enable a
+              sustainable ecosystem.
+            </p>
           </div>
 
           <div className="col-xs-6 col-md-3">
@@ -26,39 +31,64 @@ export default function Footer( ) {
           <div className="col-xs-6 col-md-3">
             <h6>Quick Links</h6>
             <ul className="footer-links">
-              <li><a href="/about/">About Us</a></li>
-              <li><a href="/contact/">Contact Us</a></li>
-              <li><a href="https://github.com/abhisekmaiti19/hackAduck">Contribute</a></li>
-              <li><a href="/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="https://goo.gl/maps/WEz3axuP8hSxRQ7NA">Location</a></li>
+              <li>
+                <a href="/about/">About Us</a>
+              </li>
+              <li>
+                <a href="/contact/">Contact Us</a>
+              </li>
+              <li>
+                <a href="https://github.com/abhisekmaiti19/hackAduck">
+                  Contribute
+                </a>
+              </li>
+              <li>
+                <a href="/privacy-policy/">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="https://goo.gl/maps/WEz3axuP8hSxRQ7NA">Location</a>
+              </li>
             </ul>
           </div>
         </div>
-        <hr/>
+        <hr />
       </div>
       <div className="container">
         <div className="row">
           <div className="col-md-8 col-sm-6 col-xs-12">
-            <p className="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
-<<<<<<< HEAD
-         <a href="#"> Quick Store</a>. Develope with ❤ by <a href="https://abhisekmaiti19.me"> Abhisek Maiti (techkydevs) </a>
-=======
-         <a href="#"> Quick Store</a>. Develope with ❤ by <a href="https://abhisekmaiti19.me"> Abhisek Maiti </a>
->>>>>>> 8567ff5c189267f155cf9c216e351a439472fec6
+            <p className="copyright-text">
+              Copyright &copy; 2020 All Rights Reserved by
+              <a href="#"> Quick Store</a>. Develope with ❤ by{" "}
+              <a href="https://abhisekmaiti19.me"> Abhisek Maiti </a>
             </p>
           </div>
 
           <div className="col-md-4 col-sm-6 col-xs-12">
             <ul className="social-icons">
-              <li><a className="facebook" href="https://facebook.com/techkydevs"><FaFacebook/></a></li>
-              <li><a className="github" href="https://github.com/abhisekmaiti19"><FaGithub/></a></li>
-              <li><a className="discord" href="https://discord.gg/GvGED8YAbz"><FaDiscord/></a></li>
-              <li><a className="globe" href="https://techkydevs.com"><FaGlobe/></a></li> 
+              <li>
+                <a className="facebook" href="https://facebook.com/techkydevs">
+                  <FaFacebook />
+                </a>
+              </li>
+              <li>
+                <a className="github" href="https://github.com/abhisekmaiti19">
+                  <FaGithub />
+                </a>
+              </li>
+              <li>
+                <a className="discord" href="https://discord.gg/GvGED8YAbz">
+                  <FaDiscord />
+                </a>
+              </li>
+              <li>
+                <a className="globe" href="https://techkydevs.com">
+                  <FaGlobe />
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
- </footer>
-    
+    </footer>
   )
 }

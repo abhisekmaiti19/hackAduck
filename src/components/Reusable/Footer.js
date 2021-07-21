6e16ba54024a84d0660468818c7b70bbc3fc977e
Abhisek Maiti <abhisekmaiti19@gmail.com>
2021-07-22 02:11:51 +0530

    Change 2021

--- src/components/Reusable/Footer.js
+++ src/components/Reusable/Footer.js
@@ -57,7 +57,7 @@ export default function Footer() {
         <div className="row">
           <div className="col-md-8 col-sm-6 col-xs-12">
             <p className="copyright-text">
-              Copyright &copy; 2020 All Rights Reserved by
+              Copyright &copy; 2021 All Rights Reserved by
               <a href="#"> Quick Store</a>. Develope with ❤ by{" "}
               <a href="https://abhisekmaiti19.me"> Abhisek Maiti </a>
             </p>
